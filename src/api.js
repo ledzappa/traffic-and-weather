7c5c2cf2702fa15cf195c9be1fb54da760f59aaf
Mikael Juntti <juntti.mikael@gmail.com>
2019-06-10 22:40:09 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,68 @@
+const Http = new XMLHttpRequest();
+const apiKey = "d2de489f04724cc29c9d855cef1051b1";
+let numberOfRequests = 0;
+let timerSet = false;
+let interval = null;
+const url =
+  "http://api.sl.se/api2/realtimedeparturesV4.json?key=" +
+  apiKey +
+  "&siteid=1860&timewindow=60";
+start();
+
+function sendRequest() {
+  document.getElementById("content").innerHTML = "Laddar ...";
+  Http.open("GET", url);
+  Http.send();
+  numberOfRequests++;
+}
+
+function getHours() {
+  const d = new Date();
+  return d.getHours();
+}
+
+function start() {
+  sendRequest();
+
+  if (!interval) {
+    interval = setInterval(function() {
+      sendRequest();
+    }, 10000);
+  }
+
+  if (!timerSet) {
+    timerSet = true;
+    setTimeout(() => {
+      clearInterval(interval);
+      timerSet = false;
+      interval = null;
+      document.getElementById("content").innerHTML = "<h1>Uppdatera</h1>";
+    }, 60000 * 10);
+  }
+}
+
+Http.onreadystatechange = e => {
+  if (e.currentTarget.readyState == 4) {
+    let response = JSON.parse(Http.responseText);
+    let buses = response.ResponseData.Buses;
+
+    gullmars = buses.filter(obj => obj.Destination === "Gullmarsplan");
+    alvsjo = buses.filter(obj => obj.Destination !== "Gullmarsplan");
+
+    document.getElementById("content").innerHTML =
+      "<h2>Gullmarsplan</h2><h1>" +
+      gullmars[0].DisplayTime +
+      "</h1>" +
+      gullmars[1].DisplayTime +
+      "<br>" +
+      gullmars[2].DisplayTime +
+      "<br>" +
+      "<h2>Älvsjö</h2><h1>" +
+      alvsjo[0].DisplayTime +
+      "</h1>" +
+      alvsjo[1].DisplayTime +
+      "<br>" +
+      alvsjo[2].DisplayTime +
+      "<br>";
+  }
+};
