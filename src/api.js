cf524201fe01b23d02ebdde6ba63e7e35f02426d
Mikael Juntti <juntti.mikael@gmail.com>
2019-06-11 22:02:55 +0200

    added temperature info

--- src/api.js
+++ src/api.js
@@ -1,18 +1,30 @@
 const Http = new XMLHttpRequest();
+const Http2 = new XMLHttpRequest();
 const apiKey = "d2de489f04724cc29c9d855cef1051b1";
+const apiKeyWeather = "d522aa97197fd864d36b418f39ebb323";
 let numberOfRequests = 0;
 let timerSet = false;
 let interval = null;
+let temp = 0;
 const url =
   "http://api.sl.se/api2/realtimedeparturesV4.json?key=" +
   apiKey +
   "&siteid=1860&timewindow=60";
+const tempUrl = 'https://api.weather.com/v2/turbo/vt1observation?apiKey=' + apiKeyWeather + '&format=json&geocode=59.33%2C18.07&language=en-US&units=m';
+
 start();
 
-function sendRequest() {
-  document.getElementById("content").innerHTML = "Laddar ...";
+function getSLinfo() {
+  document.getElementById("content-sl").innerHTML = "Laddar ...";
   Http.open("GET", url);
   Http.send();
+  numberOfRequests++;
+}
+
+function getTemp() {
+  document.getElementById("temp").innerHTML = "Laddar ...";
+  Http2.open("GET", tempUrl);
+  Http2.send();
   numberOfRequests++;
 }
 
@@ -22,11 +34,13 @@ function getHours() {
 }
 
 function start() {
-  sendRequest();
-
+  getSLinfo();
+  getTemp();
+  isItFriday();
+  
   if (!interval) {
     interval = setInterval(function() {
-      sendRequest();
+      getSLinfo();
     }, 10000);
   }
 
@@ -41,6 +55,18 @@ function start() {
   }
 }
 
+function isItFriday() {
+  let d = new Date();
+  let text = '';
+  if (d.getDay() === 2) {
+    text = (temp > 15 ? 'OCH' : 'MEN' ) + ' DET ÄR FREDAG! :D :D :D';
+  } else {
+    text = 'Det är inte fredag :(';
+  }
+  document.getElementById("friday").innerHTML = text;
+  console.log(d.getDay());
+}
+
 Http.onreadystatechange = e => {
   if (e.currentTarget.readyState == 4) {
     let response = JSON.parse(Http.responseText);
@@ -49,7 +75,7 @@ Http.onreadystatechange = e => {
     gullmars = buses.filter(obj => obj.Destination === "Gullmarsplan");
     alvsjo = buses.filter(obj => obj.Destination !== "Gullmarsplan");
 
-    document.getElementById("content").innerHTML =
+    document.getElementById("content-sl").innerHTML =
       "<h2>Gullmarsplan</h2><h1>" +
       gullmars[0].DisplayTime +
       "</h1>" +
@@ -64,5 +90,13 @@ Http.onreadystatechange = e => {
       "<br>" +
       alvsjo[2].DisplayTime +
       "<br>";
+  }
+};
+
+Http2.onreadystatechange = e => {
+  if (e.currentTarget.readyState == 4) {
+    let response = JSON.parse(Http2.responseText);
+    temp = response.vt1observation.temperature;
+    document.getElementById("temp").innerHTML = '<b>' + temp + '</b> jävla grader är det!';
   }
 };
