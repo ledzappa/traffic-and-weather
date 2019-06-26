84ca64515186e6f3b5852c2a92bbc24ce4e236db
Mikael Juntti <juntti.mikael@gmail.com>
2019-06-26 23:37:03 +0200

    nicer layout, better code, wip

--- src/script.js
+++ src/script.js
@@ -1,29 +1,30 @@
-
-const Http = new XMLHttpRequest();
-const Http2 = new XMLHttpRequest();
-
 let numberOfRequests = 0;
 let timerSet = false;
 let interval = null;
 let temp = 0;
-const url =
+const trafficUrl =
   "http://api.sl.se/api2/realtimedeparturesV4.json?key=" +
   apiKeys.sl +
   "&siteid=1860&timewindow=60";
-const tempUrl = 'https://api.weather.com/v2/turbo/vt1observation?apiKey=' + apiKeys.weather + '&format=json&geocode=59.33%2C18.07&language=en-US&units=m';
-
+const temperatureUrl =
+  "https://api.weather.com/v2/turbo/vt1observation?apiKey=" +
+  apiKeys.weather +
+  "&format=json&geocode=59.33%2C18.07&language=en-US&units=m";
+const Http = new XMLHttpRequest();
+const Http2 = new XMLHttpRequest();
 start();
 
 function getSLinfo() {
-  document.getElementById("content-sl").innerHTML = "Laddar ...";
-  Http.open("GET", url);
+  this.getTime();
+  // document.querySelector(".content").innerHTML = "Laddar ...";
+  Http.open("GET", trafficUrl);
   Http.send();
   numberOfRequests++;
 }
 
 function getTemp() {
   document.getElementById("temp").innerHTML = "Laddar ...";
-  Http2.open("GET", tempUrl);
+  Http2.open("GET", temperatureUrl);
   Http2.send();
   numberOfRequests++;
 }
@@ -36,11 +37,13 @@ function getHours() {
 function start() {
   getSLinfo();
   getTemp();
+  getTime();
   isItFriday();
-  
+
   if (!interval) {
     interval = setInterval(function() {
       getSLinfo();
+      getTime();
     }, 10000);
   }
 
@@ -55,41 +58,47 @@ function start() {
   }
 }
 
+function getTime() {
+  const d = new Date();
+  document.getElementById("last-update").innerHTML =
+    "Uppdaterad: " + d.toLocaleTimeString();
+}
+
 function isItFriday() {
   let d = new Date();
-  let text = '';
+  let text = "";
   if (d.getDay() === 5) {
-    text = (temp > 15 ? 'OCH' : 'MEN' ) + ' DET ÄR FREDAG! :D :D :D';
+    text = (temp > 15 ? "OCH" : "MEN") + " DET ÄR FREDAG! :D :D :D";
   } else {
-    text = 'Det är inte fredag :(';
+    text = "Det är inte fredag :(";
   }
   document.getElementById("friday").innerHTML = text;
   console.log(d.getDay());
+}
+
+function getTrafficTimes(array) {
+  let output = "";
+  for (let [index, item] of array.entries()) {
+    output +=
+      index === 0
+        ? "<h1>" + item.DisplayTime + "</h1><h4>"
+        : item.DisplayTime +
+          (index !== array.length - 1 ? "&nbsp;&nbsp;&nbsp;" : "");
+  }
+  output += "</h4>";
+  return output;
 }
 
 Http.onreadystatechange = e => {
   if (e.currentTarget.readyState == 4) {
     let response = JSON.parse(Http.responseText);
     let buses = response.ResponseData.Buses;
-
     gullmars = buses.filter(obj => obj.Destination === "Gullmarsplan");
     alvsjo = buses.filter(obj => obj.Destination !== "Gullmarsplan");
-
-    document.getElementById("content-sl").innerHTML =
-      "<h2>Gullmarsplan</h2><h1>" +
-      gullmars[0].DisplayTime +
-      "</h1>" +
-      gullmars[1].DisplayTime +
-      "<br>" +
-      gullmars[2].DisplayTime +
-      "<br>" +
-      "<h2>Älvsjö</h2><h1>" +
-      alvsjo[0].DisplayTime +
-      "</h1>" +
-      alvsjo[1].DisplayTime +
-      "<br>" +
-      alvsjo[2].DisplayTime +
-      "<br>";
+    gullmarsOutput = "<h2>Gullmarsplan</h2>" + getTrafficTimes(gullmars);
+    alvsjoOutput = "<h2>Älvsjö</h2>" + getTrafficTimes(alvsjo);
+    document.getElementById("gullmars").innerHTML = gullmarsOutput;
+    document.getElementById("alvsjo").innerHTML = alvsjoOutput;
   }
 };
 
@@ -97,6 +106,7 @@ Http2.onreadystatechange = e => {
   if (e.currentTarget.readyState == 4) {
     let response = JSON.parse(Http2.responseText);
     temp = response.vt1observation.temperature;
-    document.getElementById("temp").innerHTML = '<b>' + temp + '</b> jävla grader är det!';
+    document.getElementById("temp").innerHTML =
+      "<b>" + temp + "</b> jävla grader är det!";
   }
 };
