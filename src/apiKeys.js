c59c32117efdd91ed6eff297f4fdb6868c97c36d
Mikael Juntti <juntti.mikael@gmail.com>
2019-09-21 19:23:40 +0200

    gh-pages

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+const apiKeys = {
+  slRealtime: "d2de489f04724cc29c9d855cef1051b1",
+  slDeviations: "7241b80897fc4f7289ec738f481a2215",
+  weather: "d522aa97197fd864d36b418f39ebb323",
+  giphy: "In5JKoBFt0IN0Ylr1vDbtmPW1nIDNnbk"
+};
